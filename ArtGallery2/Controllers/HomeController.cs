a7baa769e6346c320bd798ecc424f90147f4b2f2
Jeremy Landi <Jeremy Landi>
2016-05-24 12:09:01 -0700

    added digital print view

--- ArtGallery2/Controllers/HomeController.cs
+++ ArtGallery2/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using ArtGallery2.Models;
+using ArtGallery2.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,14 +10,46 @@ namespace ArtGallery2.Controllers
 {
     public class HomeController : Controller
     {     
+        ArtGalleryDbContext _context = new ArtGalleryDbContext();
+
         // GET: Home
         public ActionResult Index()
         {
-            ArtGalleryDbContext _context = new ArtGalleryDbContext();
+            return View();
+        }
 
-            var listOfArt = (from aw in _context.ArtWork
-                             select aw).ToList();
-            return View(listOfArt);
+        public ActionResult DigitalPrint()
+        {
+            var listOfDigitalPrint = (from ip in _context.IndividualPiece
+                                      where ip.Category == "Digital Print"
+
+                                      join aw in _context.ArtWork
+                                      on ip.ArtWorkId equals aw.ArtWorkId
+                                      group ip by new
+                                      {
+                                          ip.Image,
+                                          aw.ArtWorkId,
+                                          ip.Price,
+                                          aw.Dimensions,
+                                          ip.Location,
+                                          aw.NumberInInventory,
+                                          aw.Title
+                                      }
+                                      into g  
+
+                             select new DigitalPrintViewModel
+                             {
+                                 Image = g.Key.Image,
+                                 ArtWorkId = g.Key.ArtWorkId,
+                                 Price = g.Key.Price,
+                                 Dimensions = g.Key.Dimensions,
+                                 Location = g.Key.Location,
+                                 NumberInInventory = g.Key.NumberInInventory,
+                                 Title = g.Key.Title
+
+                             }).ToList();
+
+            return View(listOfDigitalPrint);
         }
     }
 }
